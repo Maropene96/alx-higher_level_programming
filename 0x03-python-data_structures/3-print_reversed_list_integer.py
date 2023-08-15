d7d24848f9f9dec10865e5fce5f370120e54d347
Maropene96 <rashetlemaropene@gmail.com>
2023-08-15 11:57:12 -0700

    structures

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if idx < 0:
+        return my_list
+    if  idx > len(my_list) - 1:
+        return my_list
+    else:
+        my_list.reverse()
+        or i in range(len(my_list)):
+            print("{:d}".format(my_list[i]))
+
