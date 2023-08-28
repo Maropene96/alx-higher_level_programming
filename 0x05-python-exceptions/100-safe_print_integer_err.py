1b9b3dd797b78acca646cea78dc2069afe7869e5
Maropene96 <rashetlemaropene@gmail.com>
2023-08-28 04:38:46 -0700

    except

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,5 +5,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return (True)
     except (ValueError, TypeError):
-        print("Exception: {}\n".format(sys.exc_info()[1]), file=sys.stderr)
+        print("sys.stderr.write("Exception: {}\n".format(i"))
         return (False)
