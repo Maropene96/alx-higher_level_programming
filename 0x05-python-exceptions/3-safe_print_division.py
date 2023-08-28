07a4041cbd92f46cfd46bd99e115eda5e42d0edc
Maropene96 <rashetlemaropene@gmail.com>
2023-08-28 04:05:54 -0700

    except

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,5 +6,5 @@ def safe_print_division(a, b):
     except (ZeroDivisionError, TypeError):
         result = None
     finally:
-        print("Inside result: {}".format(div))
+        print("Inside result: {}".format(result))
     return (result)
