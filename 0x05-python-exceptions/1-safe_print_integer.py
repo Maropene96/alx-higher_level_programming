94d14e56410cc89b79296a219c27e1f118f210eb
Maropene96 <rashetlemaropene@gmail.com>
2023-08-28 03:45:06 -0700

    exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,6 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except(TypeError, ValueError):
         return (True)
-    else:
+    except (ValueError, TypeError):
         return (False)
