7b53f609b367750125ed53e0479b5a5852122038
Maropene96 <rashetlemaropene@gmail.com>
2023-08-30 14:02:30 -0700

    loops

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,35 +1,47 @@
 #include "lists.h"
 
 /**
- * insert_node - Inserts a new node
+ * insert_node - Inserts a  node
  * @head:head of the linked list.
  * @number: The number 
  * Return: the address of the new node
- *or NULL if it fals
+ *or NULL if it fails
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *node; 
 	listint_t *new;
-	listint_t head;
+	listint_t *h;
+	listint_t *h_prev;
 
+	h = *head;
 	new = malloc(sizeof(listint_t));
+
 	if (new == NULL)
 		return (NULL);
-	new->n = number;
 
-	if (node == NULL || node->n >= number)
+	while (h != NULL)
 	{
-		new->next = node;
-		*head = new;
-		return (new);
+		if (h->n > number)
+			break;
+		h_prev = h;
+		h = h->next;
 	}
 
-	while (node && node->next && node->next->n < number)
-		node = node->next;
+	new->n = number;
 
-	new->next = node->next;
-	node->next = new;
+	if (*head == NULL)
+	{
+		new->next = NULL;
+		*head = new;
+	}
+	else
+	{
+		new->next = h;
+		if (h == *head)
+			*head = new;
+		else
+			h_prev->next = new;
+	}
 
 	return (new);
 }
--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,10 +1,14 @@
+#ifndef LISTS_H
+#define LISTS_H
+
 #include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
  * @n: integer
- * @next: next node
- * Description: singly linked list node structure
+ * @next: points next node
+ *
+ * Description for alx project
  */
 typedef struct listint_s
 {
@@ -12,9 +16,10 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-listint_t *add_nodeint_end(listint_t **head, const int n);
-listint_t *insert_node(listint_t **head, int number);
-void free_listint(listint_t *head);
 size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
+listint_t *insert_node(listint_t **head, int number);
 
 #endif
