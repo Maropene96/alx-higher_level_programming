2cc46a7f13fbd37f3df5056cc776a5eccd5a7fd3
Maropene96 <rashetlemaropene@gmail.com>
2023-08-30 04:31:46 -0700

    else

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for char in range(97, 123):
     if char != 101 and char != 113:
-        print("{:t}".format(chr(char + ord("a"))), end="")
+        print("{:s}".format(chr(char + ord("a"))), end="")
